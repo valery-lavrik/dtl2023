0149c15f2fb575e8d02ac7f6cf905bef881a8f0b
Воробьев Борис <bvorobev@bcs.local>
2023-04-14 13:38:30 +0300

    Ябнул шапку

--- src/components/PageWrapp/index.js
+++ src/components/PageWrapp/index.js
@@ -1,45 +1,34 @@
 import React from 'react';
-import { Outlet, useNavigate } from "react-router-dom";
-import { useAuth } from './../../containers/Auth/helpers';
+import {Outlet, useNavigate} from "react-router-dom";
+import {useAuth} from './../../containers/Auth/helpers';
 import logo from './../../assets/img/logo.svg';
 import './index.scss';
 
 
-
 export default function PageWrapp() {
-	const auth = useAuth();
-	let navigate = useNavigate();
-	const isAuth = !!auth?.user?.bearer;
+    const auth = useAuth();
+    let navigate = useNavigate();
+    const isAuth = !!auth?.user?.bearer;
 
 
-	return (
-		<div>
-			<div style={{ border: '1px solid red', background: '#bbbdbb' }}>
+    return (
+        <div>
+            <header className="head">
 
-				<img src={logo} />
-				<br />
+                <img src={logo} className="head__logo"/>
 
-				HEADER BLYAT!
-
-				{!!isAuth ? (
-					<div>
-						<p>NAME: {auth?.user?.fio}</p>
-
-						<p>
-							<button onClick={(e) => {
-								e.preventDefault();
-								auth.signout(() => navigate('/', { replace: true }));
-							}}>Выйти</button>
-						</p>
-
-					</div>
-				) : (
-					<div>
-						Не авторизован
-					</div>
-				)}
-			</div>
-			<Outlet />
-		</div >
-	);
+                {!!isAuth ? (
+                    <div className="head__profile">
+                        <div className="head__name">{auth?.user?.fio}</div>
+                        <div className="head__logout" onClick={(e) => {
+                            e.preventDefault();
+                            auth.signout(() => navigate('/', {replace: true}));
+                        }}>Выйти
+                        </div>
+                    </div>
+                ) : ''}
+            </header>
+            <Outlet/>
+        </div>
+    );
 }
