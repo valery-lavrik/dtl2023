bcd0ed6009e876a822421c7765cb6e0409818860
bvorobev <bvorobev@breez.ru>
2023-04-17 10:53:06 +0300

    BG +alt

--- src/components/BackgroundComponent.js
+++ src/components/BackgroundComponent.js
@@ -7,7 +7,7 @@ import bg from './../assets/img/graph-logo-2.jpg'
 export default function BackgroundComponent() {
     return (
         <div className="bg">
-            <img src={bg} className="bg__image"/>
+            <img alt="bg" src={bg} className="bg__image"/>
             {/* <HighchartsReact
 				highcharts={Highcharts}
 				options={options}
