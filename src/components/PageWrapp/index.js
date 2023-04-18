d19a20f6fc8f9e01e606d656f98fee9795bf39e9
lavrik acer <valery.lavrik@gmail.com>
2023-04-18 12:09:56 +0300

    Перенес табы

--- src/components/PageWrapp/index.js
+++ src/components/PageWrapp/index.js
@@ -1,47 +1,42 @@
 import React from 'react';
-import {Outlet, useNavigate, useLocation} from "react-router-dom";
-import {useAuth} from './../../containers/Auth/helpers';
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { useAuth } from './../../containers/Auth/helpers';
 import logo from './../../assets/img/logo.svg';
 import BackgroundComponent from './../../components/BackgroundComponent';
 import './index.scss';
 
 
 export default function PageWrapp() {
-    const auth = useAuth();
-    let navigate = useNavigate();
-    let location = useLocation();
-    const isAuth = !!auth?.user?.bearer;
+	const auth = useAuth();
+	let navigate = useNavigate();
+	let location = useLocation();
+	const isAuth = !!auth?.user?.bearer;
 
 
-    return (
-        <>
-            {/* Вот компанента которая должна быть на фоне только на экране авторизации */}
-            {location.pathname === '/auth' && (
-                <BackgroundComponent/>
-            )}
-            <header className={'head' + (isAuth ? ' _lk' : '')}>
+	return (
+		<>
+			{/* Вот компанента которая должна быть на фоне только на экране авторизации */}
+			{location.pathname === '/auth' && (
+				<BackgroundComponent />
+			)}
 
-                <img src={logo} alt="logo" className="head__logo"/>
+			<header className={'head' + (isAuth ? ' _lk' : '')}>
 
-                {!!isAuth ? (
-                    <div className="head__profile">
-                        <div className="head__name">{auth?.user?.fio}</div>
-                        <div className="head__logout" onClick={(e) => {
-                            e.preventDefault();
-                            auth.signout(() => navigate('/', {replace: true}));
-                        }}>Выйти
-                        </div>
-                    </div>
-                ) : ''}
-            </header>
-            {isAuth && (
-                <nav className="tabs">
-                    <div className="tabs__item">Парадная</div>
-                    <div className="tabs__item _selected">Шоколадная</div>
-                    <div className="tabs__item">Виноградная</div>
-                </nav>
-            )}
-            <Outlet/>
-        </>
-    );
+				<img src={logo} alt="logo" className="head__logo" />
+
+				{!!isAuth ? (
+					<div className="head__profile">
+						<div className="head__name">{auth?.user?.fio}</div>
+						<div className="head__logout" onClick={(e) => {
+							e.preventDefault();
+							auth.signout(() => navigate('/', { replace: true }));
+						}}>Выйти
+						</div>
+					</div>
+				) : ''}
+			</header>
+
+			<Outlet />
+		</>
+	);
 }
--- src/containers/Start/index.js
+++ src/containers/Start/index.js
@@ -8,87 +8,108 @@ import React from 'react';
 
 
 export default function Start() {
-    // const auth = useAuth();
+	// const auth = useAuth();
 
-    const cards = [{
-        name: 'Тепловой датчик',
-        status: '1',
-    }, {
-        name: 'Датчик ветра',
-        status: '2',
-    }, {
-        name: 'Элетрический датчик',
-        status: '1',
-    }, {
-        name: 'Датчик дыма',
-        status: '2',
-    }, {
-        name: 'Датчик качества кода',
-        status: '1',
-    }, {
-        name: 'Датчик шторма',
-        status: '2',
-    }, {
-        name: 'Дискретный датчик',
-        status: '1',
-    }, {
-        name: 'Датчик влажности',
-        status: '2',
-    }, {
-        name: 'Ректальный датчик',
-        status: '3',
-    }, {
-        name: 'Цифровой датчик',
-        status: '1',
-    }, {
-        name: 'Пожарный датчик',
-        status: '2',
-    }, {
-        name: 'Одномерный датчик',
-        status: '1',
-    }, {
-        name: 'Аналоговый датчик',
-        status: '1',
-    }, {
-        name: 'Одноступенчатый датчик',
-        status: '1',
-    }, {
-        name: 'Проводной датчик',
-        status: '1',
-    }, {
-        name: 'Индуктивный датчик',
-        status: '1',
-    }, {
-        name: 'Оптический датчик',
-        status: '1',
-    }, {
-        name: 'Потенциометрический датчик',
-        status: '1',
-    }, {
-        name: 'Датчик давления',
-        status: '3',
-    }, {
-        name: 'Датчик нагрузки',
-        status: '2',
-    }, {
-        name: 'Перепадомер',
-        status: '3',
-    }, {
-        name: 'Датчик гравитационных колебаний',
-        status: '2',
-    }, {
-        name: 'Радарный датчик',
-        status: '1',
-    }];
+	const cards = [{
+		name: 'Тепловой датчик',
+		status: '1',
+	}, {
+		name: 'Датчик ветра',
+		status: '2',
+	}, {
+		name: 'Элетрический датчик',
+		status: '1',
+	}, {
+		name: 'Датчик дыма',
+		status: '2',
+	}, {
+		name: 'Датчик качества кода',
+		status: '1',
+	}, {
+		name: 'Датчик шторма',
+		status: '2',
+	}, {
+		name: 'Дискретный датчик',
+		status: '1',
+	}, {
+		name: 'Датчик влажности',
+		status: '2',
+	}, {
+		name: 'Ректальный датчик',
+		status: '3',
+	}, {
+		name: 'Цифровой датчик',
+		status: '1',
+	}, {
+		name: 'Пожарный датчик',
+		status: '2',
+	}, {
+		name: 'Одномерный датчик',
+		status: '1',
+	}, {
+		name: 'Аналоговый датчик',
+		status: '1',
+	}, {
+		name: 'Одноступенчатый датчик',
+		status: '1',
+	}, {
+		name: 'Проводной датчик',
+		status: '1',
+	}, {
+		name: 'Индуктивный датчик',
+		status: '1',
+	}, {
+		name: 'Оптический датчик',
+		status: '1',
+	}, {
+		name: 'Потенциометрический датчик',
+		status: '1',
+	}, {
+		name: 'Датчик давления',
+		status: '3',
+	}, {
+		name: 'Датчик нагрузки',
+		status: '2',
+	}, {
+		name: 'Перепадомер',
+		status: '3',
+	}, {
+		name: 'Датчик гравитационных колебаний',
+		status: '2',
+	}, {
+		name: 'Радарный датчик',
+		status: '1',
+	}];
 
-    return (
-        <section className="cards">
-            {cards.map((card, i) => {
-                return <div className={"cards__item _" + card.status}>
-                    <div className="cards__id">{i + 1}</div>
-                    <div className="cards__name">{card.name}</div>
-                </div>
-            })}
-        </section>
-    );
+
+	const onPress = (card) => {
+		console.log('card', card);
+	}
+
+	return (
+		<>
+
+			{/* ТАБЫ */}
+			<nav className="tabs">
+				Подразделения: 
+				<div className="tabs__item">Цех 1</div>
+				<div className="tabs__item _selected">Цех 2</div>
+				<div className="tabs__item">Цех 3</div>
+			</nav>
+
+
+			<section className="cards">
+				{cards.map((card, i) => (
+					<div
+						key={i}
+						className={"cards__item _" + card.status}
+						onClick={() => onPress(card)}
+					>
+						<div className="cards__id">{i + 1}</div>
+						<div className="cards__name">{card.name}</div>
+					</div>
+				))}
+			</section>
+		</>
+	);
 }
